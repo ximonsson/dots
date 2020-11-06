4b625e2a34c4a7a449853c7b4b20bf68ae34b1e5
Simon Simonsson <simonsson.simon@gmail.com>
2020-11-06 10:48:44 +0100

    improvements to plotting theme

--- julia/makie-theme.jl
+++ julia/makie-theme.jl
@@ -1,22 +1,36 @@
 begin
-	local gridcolor = RGB(.5, .5, .5)
-	local textsize = 2.5
+	local gridcolor = RGB(.25, .25, .25)
+	local textsize = 1.5
+	local tickcolor = RGB(.5, .5, .5)
 	local labelcolor = RGB(.8, .8, .8)
 
 	Attributes(
+		resolution = (1200, 800),
 		backgroundcolor = RGB(.15, .15, .15),
-		color = RGB(.9, .9, .9),
+		color = RGB(1., 1., 1.),
+		padding = (0, 0, 0),
+		show_legend = true,
+		title = Attributes(
+			textsize = 20,
+		),
 		axis = Attributes(
 			grid = Attributes(
 				linecolor = (gridcolor, gridcolor),
 			),
 			ticks = Attributes(
 				textsize = (textsize, textsize),
-				textcolor = (gridcolor, gridcolor),
+				textcolor = (tickcolor, tickcolor),
+				linecolor = (labelcolor, labelcolor),
+				gap = 1,
+				title_gap = 5,
 			),
 			names = Attributes(
 				textcolor = (labelcolor, labelcolor),
-				textsize = (3, 3),
+				textsize = (2, 2),
+			),
+			frame = Attributes(
+				frames = ((true, false), (true, false)),
+				linecolor = RGB(.3, .3, .3),
 			),
 		),
 	)
--- julia/startup.jl
+++ julia/startup.jl
@@ -2,7 +2,9 @@ using Plots, Plots.PlotMeasures
 
 """ Set default plot theme """
 
-const PLOT_THEME = (
+begin
+
+local PLOT_THEME = (
 	background = RGB(.15, .15, .15),
 	margin = 30px,
 	bottom_margin = 50px,
@@ -13,8 +15,14 @@ const PLOT_THEME = (
 	foreground_color_border = RGB(.3, .3, .3),
 	foreground_color_legend = RGBA(.5, .5, .5, 0),
 	tickfontcolor = RGB(.5, .5, .5),
+	foreground_color_subplot = RGB(.5, .5, .5),
+	titlefontcolor = RGB(1, 1, 1),
+	palette = [RGB(1, 1, 1), RGB(1, .3, .3), RGB(.8, .6, .4), RGB(.5, 1, .5), RGB(.3, .7, 1)],
 	legend = :topleft,
 	size = (1200, 700),
+	fillalpha = .3,
 )
 
 default(; PLOT_THEME...)
+
+end
