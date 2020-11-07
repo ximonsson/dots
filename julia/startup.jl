12478cb59cc1285496b58df21fe1d1985d503c96
Simon Simonsson <simonsson.simon@gmail.com>
2020-11-07 17:52:05 +0100

    fixing font sizes and colors in axis

--- julia/startup.jl
+++ julia/startup.jl
@@ -4,22 +4,29 @@ using Plots, Plots.PlotMeasures
 
 begin
 
+@info "setting plot theme"
+
 local PLOT_THEME = (
 	background = RGB(.15, .15, .15),
-	margin = 30px,
-	bottom_margin = 50px,
-	top_margin = 70px,
+	margin = 10px,
+	bottom_margin = 20px,
+	top_margin = 30px,
 	title_location = :left,
 	legendfontsize = 5,
+	titlefontsize = 10,
+	#guidefontsize = 7, # these do not work
+	#tickfontsize = 6, # these do not work
+	guidefont = font(7, RGB(.8, .8, .8)),
+	tickfont = font(6, RGB(.5, .5, .5)),
 	background_color_legend = RGBA(.15, .15, .15, .7),
 	foreground_color_border = RGB(.3, .3, .3),
 	foreground_color_legend = RGBA(.5, .5, .5, 0),
 	tickfontcolor = RGB(.5, .5, .5),
-	foreground_color_subplot = RGB(.5, .5, .5),
+	foreground_color_subplot = RGB(.6, .6, .6),
 	titlefontcolor = RGB(1, 1, 1),
-	palette = [RGB(1, 1, 1), RGB(1, .3, .3), RGB(.8, .6, .4), RGB(.5, 1, .5), RGB(.3, .7, 1)],
+	palette = [RGB(1, 1, 1), RGB(1, .3, .3), RGB(1., .5, .2), RGB(.5, 1, .5), RGB(.3, .7, 1)],
 	legend = :topleft,
-	size = (1200, 700),
+	size = (800, 500),
 	fillalpha = .3,
 )
 
