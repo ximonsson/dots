023ef26dcfd1581009b00c6f0c998cb1146eb905
Simon Simonsson <simonsson.simon@gmail.com>
2025-08-29 02:45:31 +0200

    feat: refactor adapters configuration to use http namespace
    - Moved mlflow and warpstral adapters under the http namespace
    - Ensured proper indentation and structure for the new configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -154,50 +154,52 @@ require("codecompanion").setup({
 		},
 	},
 	adapters = {
-		mlflow = function()
-			return require("codecompanion.adapters").extend("openai_compatible", {
-				name = "code_clerk",
-				formatted_name = "Code Clerk",
+		http = {
+			mlflow = function()
+				return require("codecompanion.adapters").extend("openai_compatible", {
+					name = "code_clerk",
+					formatted_name = "Code Clerk",
 
-				env = {
-					api_key = "xxx",
-					url = "http://localhost:5000",
-				},
+					env = {
+						api_key = "xxx",
+						url = "http://localhost:5000",
+					},
 
-				opts = {
-					vision = false,
-				},
+					opts = {
+						vision = false,
+					},
 
-				handlers = {
-					---@param self CodeCompanion.Adapter
-					---@return boolean
-					setup = function(self)
-						if self.opts and self.opts.stream then
-							self.parameters.stream = true
+					handlers = {
+						---@param self CodeCompanion.Adapter
+						---@return boolean
+						setup = function(self)
+							if self.opts and self.opts.stream then
+								self.parameters.stream = true
+							end
+							return true
 						end
-						return true
-					end
-				}
-			})
-		end,
-
-		warpstral = function()
-			return require("codecompanion.adapters").extend("mistral", {
-				name = "warpstral",
-				formatted_name = "Warpstral",
-
-				env = {
-					url = "http://localhost:4000/mistral",
-					api_key = "xxx",
-				},
-
-				schema = {
-					---@type CodeCompanion.Schema
-					model = {
-						default = "devstral-small-latest",
 					}
-				}
-			})
-		end,
+				})
+			end,
+
+			warpstral = function()
+				return require("codecompanion.adapters").extend("mistral", {
+					name = "warpstral",
+					formatted_name = "Warpstral",
+
+					env = {
+						url = "http://localhost:4000/mistral",
+						api_key = "xxx",
+					},
+
+					schema = {
+						---@type CodeCompanion.Schema
+						model = {
+							default = "devstral-small-latest",
+						}
+					}
+				})
+			end,
+		},
 	}
 })
